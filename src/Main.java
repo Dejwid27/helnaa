7ec90b2b5f82e70ef242f8906e4e7ed8a7b2eeb7
Dejwid27 <dawidpiejjo@gmail.com>
2024-09-13 11:36:27 +0200

    zaddom

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,43 @@ import java.util.*;
 public class Main {
 
 
-
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Gra gra1 = new Gra();
         gra1.zagraj();
+        System.out.println("pierwsze" + sitoEratosferesa(200));
+    }
+
+    /* do kartkowki
+
+     */
+    static ArrayList<Integer> sitoEratosferesa(int liczba) {
+        //orzgitiwanie listy z wartosciami logicznymi
+        ArrayList<Boolean> logiczne = new ArrayList<>();
+        logiczne.add(false);
+        logiczne.add(false);
+        for (int i = 0; i < liczba; i++) {
+            logiczne.add(true);
+        }
+        //wykreslanie wielokrotnosci liczb pierwszych
+        for (int i = 0; i < liczba; i++) {
+            if (logiczne.get(i).equals(true)) {
+                //wykreslanie zmiana na false
+                for (int k = i * 2; k < liczba; k = k + i) {
+                    logiczne.set(k, false);
+                }
+            }
+        }
+        ArrayList<Integer> pierwsze = new ArrayList<>();
+        for (int i = 0; i < logiczne.size(); i++) {
+            if (logiczne.get(i).equals(true))
+                pierwsze.add(i);
+        }
+
+        return pierwsze;
     }
 }
+
+
 
 
 
