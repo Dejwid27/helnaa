fcfae8f8ba588afc0d8d0e3c76cd963e18de9831
Dejwid27 <dawidpiejjo@gmail.com>
2024-09-13 10:10:01 +0200

    powpowpwo

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static <Linkedlist> void main(String[] args) {
+    public static <Linkedlist> LinkedList<Integer> main(String[] args) {
         System.out.println("Hello world!");
         //tablica
         /*
@@ -64,18 +61,62 @@ public class Main {
         System.out.println("Zbior liczb losowych");
         System.out.println(zbiorliczbLosowych);
         //lista liczb wczytywana z klawiatury
+
+
         Scanner Klawiatura = new Scanner(System.in);
-        Linkedlist<Integer> listaliczbKlawiatury = new Linkedlist<>();
+        LinkedList<Integer> listaliczbKlawiatury;
+        listaliczbKlawiatury = new LinkedList<>();
         System.out.println("Podaj 6 liczb");
         for (int i = 0; i <6; i++) {
             int wartosc = Klawiatura.nextInt();
             listaliczbKlawiatury.add(wartosc);
         }
 
-
-
+        return listaliczbKlawiatury;
     }
 
-    private static class integer {
+    /**
+     * wylosujLiczby - metoda ktora losuje liczby calkowite z zakresu od 1 do 100
+     * @param ile - liczba wylosowanych wartosci
+     * @return - zwracam nam liste z wylosowanymi wartosciami
+     */
+    private ArrayList<Integer> wylosujLiczby(int ile){
+        ArrayList<Integer> listaliczbLosowychBezPowtorzen = new ArrayList<>();
+        int liczba;
+        for (int i = 0; i < 6; i++) {
+            liczba = (int) (Math.random() * 100 + 1);
+            while (listaliczbLosowychBezPowtorzen.contains(liczba)) {
+                liczba = (int) (Math.random() * 100 + 1);
+            }
+            listaliczbLosowychBezPowtorzen.add(liczba);
+        }
+        return listaliczbLosowychBezPowtorzen;
+    }
+        private LinkedList<Integer> wczytajLiczby(int ile){
+            LinkedList<Integer> wpisane = new LinkedList<>();
+            Scanner klawiatura = new Scanner(System.in);
+            System.out.println("podaj "+ile+" licz");
+            for (int i = 0; i <ile ; i++) {
+                wpisane.add(klawiatura.nextInt());
+            }
+            return wpisane;
+        }
+    private void wypiszListe(List<Integer>lista){
+        for (Integer element:lista) {
+            System.out.print(element+",");
+        }
+
+    }
+    private ArrayList<Integer> sprawdzktorepowtarzajaSie(ArrayList<Integer> wpisane, LinkedList<Integer> wylosowane){
+        ArrayList<Integer> trafione = new ArrayList<>();
+        for (Integer element:wylosowane){
+            if(wpisane.contains(element)){
+                trafione.add(element);
+            }
+        }
+        return trafione;
     }
 }
+
+
+
